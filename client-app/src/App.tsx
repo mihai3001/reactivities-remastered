c893a97a21cbb3bf75513433f34d445822f7b8d6
mihai3001 <mihai.ghita3001@gmail.com>
2020-09-13 22:08:26 +0300

    End of Section 3

---
+++ client-app/src/App.tsx
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import React from 'react';
+import { Header, Icon, List } from 'semantic-ui-react';
+import './App.css';
+
+class App extends React.Component {
+
+  state = {
+    values: [],
+  }
+  componentDidMount() {
+    axios.get('http://localhost:5000/api/values')
+      .then((response) => {
+        this.setState({
+          values: response.data
+        })
+      })
+  }
+  render() {
+    return (
+      <div>
+        <Header as='h2'>
+          <Icon name='users'/>
+          <Header.Content>Reactivities</Header.Content>
+        </Header>
+        <List>
+        {this.state.values.map((value: any) => (
+            <List.Item key={value.id}>{value.name}</List.Item>
+          ))}
+        </List>
+      </div>
+    );
+  }
+}
+
+export default App;
